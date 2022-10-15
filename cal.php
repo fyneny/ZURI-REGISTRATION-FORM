d8471b344316f39d43228e7aa8b8a3e795a34e9d
fyneny <fyneny92@gmail.com>
2022-10-15 12:23:20 +0100

    initial commit

---
+++ cal.php
@@ -0,0 +1,36 @@
+<?php
+$name = $_GET['name'];
+$email =$_GET['email'];
+$birthday = $_GET['birthday'];
+$gender = $_GET['gender'];
+$country = $_GET['country'];
+
+
+echo "<h2>Your Input:</h2>";
+
+echo $fullname;
+
+echo "<br>";
+
+echo $email;
+
+echo "<br>";
+
+echo $dateofbirth;
+
+echo "<br>";
+
+echo $gender;
+
+echo "<br>";
+
+echo $country;
+
+echo "<br>";
+
+?>
+
+
+
+
+
---
+++ calculator.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Userdata</title>
+</head>
+<body>
+<form method="$_GET" action="cal.php">
+     <div class="container">  
+
+         <center>  <h1>Registeration Form</h1> </center>  
+    
+         <hr>  
+    
+         <label> <b>Fullname </b> </label>   
+    
+         <input type="text" name="name" placeholder= "Fullname" size="50" required /> <br><br>  
+    
+         <label for="email"><b>Email</b></label>  
+    
+         <input type="text" placeholder="Enter Email" name="email" required><br> <br> 
+    
+         <label for="birthday"><b>Date of Birth</b></label>  
+    
+         <input type="birthday" placeholder="Date of Birth" name="birthday" required><br><br>  
+    
+        <div>  
+    
+          <label>   
+    
+             <b>Gender : </b> 
+    
+           </label><br>  
+    
+           <input type="radio" value="Male" name="gender" checked > Male   
+    
+            <input type="radio" value="Female" name="gender"> Female   
+    
+            <input type="radio" value="Other" name="gender"> Other  
+    
+        </div> <br><br> 
+             
+             <label for="country"><b>Country</b></label>  
+    
+             <input type="country" placeholder="Country" name="Country"> <br> <br> 
+    
+             <button type="submit" class="registerbtn">SUBMIT</button> <br>  
+
+    </form>
+</body>
+</html>
